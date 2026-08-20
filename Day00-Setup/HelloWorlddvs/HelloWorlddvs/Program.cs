b5796ca35cc2e239291164910c0423b8c9204418
Shaik roshan <immortalroshan111@gmail.com>
2026-08-20 10:50:03 +0530

    Day00 : Environment setup , first console app , README

---
+++ Day00-Setup/HelloWorlddvs/HelloWorlddvs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your name? ");
+            string name = Console.ReadLine();
+
+            Console.Write("Which programming language do you want to learn? ");
+            string language = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {name}! Welcome to C#.");
+            Console.WriteLine($"You want to learn {language}. That's awesome!");
+
+            Console.WriteLine($"Today is {DateTime.Now:dddd, dd MMMM yyyy}.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
